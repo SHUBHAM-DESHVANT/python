3e6bc96836bf7d86e2b752bb99fc9ab7f2104c61
SHUBHAM-DESHVANT <deshvantshubham@gmail.com>
2024-09-28 11:34:57 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,10 @@
+student={'name': 'amruta','address':'ranjangaon'}
+print(student)
+student['address']='wagholi'
+print(student)
+#dict.clear()
+#print(dict)
+#del dict
+#print(dict)
+del student['name']
+print(student)
